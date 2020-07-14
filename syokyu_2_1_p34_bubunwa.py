f13ae4151e86f1ad7047c2b74aa09a493e8cd951
willcom1992 <takuma1992jpn@gmail.com>
2020-07-14 11:30:23 +0900

    200714

--- syokyu_2_1_p34_bubunwa.py
+++ syokyu_2_1_p34_bubunwa.py
@@ -1,3 +1,4 @@
+# ビット全探索での実装
 n = 4
 al = [1,2,4,7]
 k = 13
@@ -5,8 +6,10 @@ k = 13
 for i in range(2**n):
     sum  = 0
     for j in range(n):
+        #ビットシフトを行い、もしもビットが立っていたら和に加える
         if 1 & (i >> j):
             sum += al[j]
+            
     if sum == k:
         print('Yes')
         break
