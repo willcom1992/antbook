4468fc24c5226a543533cfe9669d14c4f5147681
willcom1992 <takuma1992jpn@gmail.com>
2020-07-20 23:17:57 +0900

    FenceRepair

--- 初級2-2_p49_FenceRepair.py
+++ 初級2-2_p49_FenceRepair.py
@@ -1,13 +1,19 @@
 import heapq
 N = 3
 L = [8, 5, 8]
+# 優先度付きキュー
 heapq.heapify(L)
 
 ans = 0
 while len(L) > 1:
+    # 最も短い板
     x = heapq.heappop(L)
+    # 次に短い板
     y = heapq.heappop(L)
+
     ans += x+y
+    # 新しくできた板を優先度付きキューに追加
     heapq.heappush(L, x + y)
 
 print(ans)
+
